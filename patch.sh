ea561226722523e2af708ac1d76541acc70c3424
Saphira Kai <kai.saphira@gmail.com>
2023-01-28 12:08:56 -0400

    initial commit

---
+++ patch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n'
+
+verbose=false
+
+for arg in $@; do
+    case $arg in
+        -v|--verbose) verbose=true
+    esac
+done
+
+[ -d profile/ ] && {
+    echo "Removing existing profile..."
+    rm -rf profile/
+}
+
+profile=releng
+echo "Copying profile $profile..."
+cp -r /usr/share/archiso/configs/$profile/ profile/
+
+echo "Applying overrides..."
+pushd override/ &>/dev/null
+find . -type f -not -name "*:diff" -exec cp {} ../profile/{} \;
+find . -type f -name "*:diff" -exec sh -c "patch -f $($verbose || echo '-s') ../profile/\$(basename {} :diff) {}" \;
+popd &>/dev/null
