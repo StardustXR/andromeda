7b3150c15764fb1fbad390349a7885985acf3015
Saphira Kai <kai.saphira@gmail.com>
2023-01-29 15:08:35 -0400

    automatically acquire Chaotic-AUR key

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 set -euo pipefail
 IFS=$'\n'
 
+chaoticaur_key=FBA220DFC880C036
+
 read -p 'update profile before building? (Y/n)' res
 [ "$res" = n ] || [ "$res" = N ] || ./patch.sh && echo
 
@@ -11,7 +13,12 @@ mkdir -p work/
 echo 'Mounting work/ as tmpfs...'
 sudo mount -t tmpfs tmpfs work/
 
+$(pacman-key --list-sigs $chaoticaur_key &>/dev/null) || {
+    echo 'PGP key for Chaotic-AUR not installed, receiving and signing...'
+    sudo pacman-key --recv-key $chaoticaur_key --keyserver keyserver.ubuntu.com
+    sudo pacman-key --lsign-key $chaoticaur_key
+}
+
 #? fallback to standard filesystem if there's less than 8GB available in tmpfs
 [ $(df work --output=avail | tail -n1) -lt 8589935 ] && sudo umount work/
-
 sudo mkarchiso -v -w work/ profile/
