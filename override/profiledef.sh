6b0dc51a40e7628c1d67029e94f2528f87ed2af0
Saphira Kai <kai.saphira@gmail.com>
2023-01-29 02:48:22 -0400

    revert to default bootmodes

--- override/profiledef.sh
+++ override/profiledef.sh
@@ -8,10 +8,9 @@ iso_application="Andromeda - XR Development Environment"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr'
-           'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp'
-           'uefi-x64.grub.esp')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
