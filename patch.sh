1b9147e76120c88c1fc93e15f260081c4551eecd
Saphira Kai <kai.saphira@gmail.com>
2023-01-29 02:08:44 -0400

    improve patch.sh

--- patch.sh
+++ patch.sh
@@ -29,10 +29,15 @@ cp -r /usr/share/archiso/configs/$profile/ profile/
 #// apply overrides and patches \\\\\\\\# 
 pushd override/ &>/dev/null
 echo "Applying overrides..."
-find . -type f -not -name "*:diff" -exec cp {} ../profile/{} \; $($verbose && echo -print)
+find . -type f -not -name "*:diff" -not -name "*:rm" -exec \
+    cp {} ../profile/{} \; $($verbose && echo -print)
 
 echo "Applying patches..."
 find . -type f -name "*:diff" -exec \
-    sh -c 'patch -fs ../profile/$(basename {} :diff) {}' \; $($verbose && echo -print)
+    sh -c 'patch -fs ../profile/$(echo {} | sed s/:diff//) {}' \; $($verbose && echo -print)
+
+echo "Applying removes..."
+find . -type f -name "*:rm" -exec \
+    sh -c  'rm ../profile/$(echo {} | sed s/:rm//)' \; $($verbose && echo -print)
 popd &>/dev/null
 #\\\\\\\\ apply overrides and patches //#
